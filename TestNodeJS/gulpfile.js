1de9879416a2b5a1dbf7bd7a1ea471c9d44e21d5
JYOTIRMAYA <jyotirmaya.khuntia@oracle.com>
2018-08-02 14:36:49 +0530

    Removed

--- TestNodeJS/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var scan = require('OSCS_NODEJS');
-var options = { dev: true };
-
-gulp.task('scan', function () {
-	scan.scandeps();
-})
--- TestNodeJS/nodeFirst.js
+++
@@ -1,1 +0,0 @@
-require('OSCS_NODEJS')();
